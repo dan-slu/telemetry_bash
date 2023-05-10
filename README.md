18f34f6fb1c3c5e85b9e77c5eb3fd80978bbc9bb
dan-slu <118234715+dan-slu@users.noreply.github.com>
2023-05-09 21:18:15 -0400

    less output

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # telemetry_bash
 A bash script for debian system to report a telemetry (cpu/memory usage and cpu temp in C) using a webhook to Discord by editing a message.
+
+URL_TOKEN.txt
+only contains a webhook url in a following format: https://discord.com/api/webhooks/{webhook.id}/{webhook.token}/messages/{message.id}
--- tel_webhook.sh
+++ tel_webhook.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 while true; do
-usage_data="$(top -b -d 20 -n 2 -i | awk '$1 == "PID" {block_num++; next} block_num == 2 {sumCPU += $9; sumMEM += $10;} END {print "CPU USAGE: " sumCPU "  MEMORY USAGE: " sumMEM}')"
+usage_data="$(top -b -d 20 -n 2 -i | awk '$1 == "PID" {block_num++; next} block_num == 2 {sumCPU += $9; sumMEM += $10;} END {print "CPU USAGE: " sumCPU "%  MEMORY USAGE: " sumMEM "%"}')"
 cpu_temp="$((cat /sys/class/thermal/thermal_zone*/temp) | sed 's/\(.\)..$/.\1°C/')"
 curr_date="$(date)"
-echo "hi"
-curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X PATCH --data "{\"content\": \"$curr_date      $usage_data      CPU temp: $cpu_temp\"}" $(cat URL_TOKEN.txt)
+curl -H "Content-Type:application/json" -X PATCH --data "{\"content\": \"$curr_date      $usage_data      CPU temp: $cpu_temp\"}" $(cat URL_TOKEN.txt)
 done
